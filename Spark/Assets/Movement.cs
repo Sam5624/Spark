0c7ccdd804abfd03f8c7d718e1c56c1eaf4dbbc0
Sam <sam23home@gmail.com>
2020-07-08 09:14:13 +0100

    Added Tilemap & Player

---
+++ Spark/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(h, v);
+
+        rb.AddForce(movement);
+    }
+}
